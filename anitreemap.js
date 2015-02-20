4a93d5121a5da16afa6957528619971685dc912d
zhouqing <zhouqing3737@gmail.com>
2015-02-20 12:33:14 -0500

    changed color selection

--- anitreemap.js
+++ anitreemap.js
@@ -24,18 +24,17 @@ function Anitreemap(inputobj,containerid,widtth,heighht,userclickfunction){
 	this.userclickfunction=userclickfunction||null;
 }
 
-Anitreemap.colorset=Array('#F0F8FF','#FAEBD7','#00FFFF','#7FFFD4','#F0FFFF','#F5F5DC','#FFE4C4','#FFEBCD',
-	'#0000FF','#8A2BE2','#A52A2A','#DEB887','#5F9EA0','#7FFF00','#D2691E','#FF7F50','#6495ED','#FFF8DC','#DC143C',
-	'#00FFFF','#00008B','#008B8B','#B8860B','#A9A9A9','#006400','#BDB76B','#8B008B','#556B2F','#FF8C00','#9932CC',
-	'#8B0000','#E9967A','#8FBC8F','#483D8B','#2F4F4F','#00CED1','#9400D3','#FF1493','#00BFFF','#696969','#1E90FF',
-	'#B22222','#FFFAF0','#228B22','#FF00FF','#DCDCDC','#F8F8FF','#FFD700','#DAA520','#808080','#008000','#ADFF2F',
-	'#F0FFF0','#FF69B4','#CD5C5C','#4B0082','#FFFFF0','#F0E68C','#E6E6FA','#FFF0F5','#FFFACD','#ADD8E6',
-	'#F08080','#E0FFFF','#FAFAD2','#D3D3D3','#90EE90','#FFA07A','#20B2AA','#87CEFA','#778899','#B0C4DE',
-	'#FFFFE0','#00FF00','#32CD32','#FAF0E6','#FF00FF','#800000','#66CDAA','#0000CD','#BA55D3','#9370DB','#3CB371',
-	'#7B68EE','#00FA9A','#48D1CC','#C71585','#191970','#F5FFFA','#FFE4E1','#FFE4B5','#FFDEAD','#000080','#FDF5E6',
-	'#808000','#6B8E23','#FFA500','#FF4500','#DA70D6','#EEE8AA','#98FB98','#AFEEEE','#DB7093','#FFEFD5','#FFDAB9',
+Anitreemap.colorset=Array('#F0F8FF','#FAEBD7','#00FFFF','#7FFFD4','#F0FFFF','#00008B','#FFE4C4','#FFEBCD',
+	'#FAF0E6','#0000FF','#A52A2A','#DEB887','#5F9EA0','#7FFF00','#D2691E','#FF7F50','#6495ED','#8B008B','#DC143C',
+	'#00FFFF','#F5F5DC','#FDF5E6','#B8860B','#A9A9A9','#AFEEEE','#BDB76B','#FFF8DC','#556B2F','#FF8C00','#9932CC',
+	'#E0FFFF','#E9967A','#8FBC8F','#483D8B','#00FF00','#00CED1','#F0E68C','#FF1493','#00BFFF','#696969','#1E90FF',
+	'#B22222','#FFFAF0','#228B22','#ADFF2F','#DCDCDC','#F8F8FF','#FFD700','#DAA520','#808080','#008000','#FF00FF',
+	'#F08080','#8B0000','#FAFAD2','#D3D3D3','#90EE90','#FFA07A','#20B2AA','#87CEFA','#FFFAFA','#B0C4DE',
+	'#FFFFE0','#2F4F4F','#32CD32','#8A2BE2','#ADFF5F','#800000','#66CDAA','#0000CD','#BA55D3','#9370DB','#3CB371',
+	'#7B68EE','#00FA9A','#48D1CC','#C71585','#191970','#F5FFFA','#000080','#FFE4B5','#FFDEAD','#FFE4E1','#008B8B',
+	'#808000','#6B8E23','#FFA500','#FF4500','#DA70D6','#EEE8AA','#98FB98','#006400','#DB7093','#FFEFD5','#FFDAB9',
 	'#CD853F','#FFC0CB','#DDA0DD','#B0E0E6','#800080','#FF0000','#BC8F8F','#4169E1','#8B4513','#FA8072','#F4A460',
-	'#2E8B57','#FFF5EE','#A0522D','#C0C0C0','#87CEEB','#6A5ACD','#708090','#FFFAFA','#00FF7F','#4682B4','#D2B48C',
+	'#2E8B57','#A0522D','#FFF5EE','#C0C0C0','#87CEEB','#6A5ACD','#708090','#778899','#00FF7F','#4682B4','#D2B48C',
 	'#008080','#D8BFD8','#FF6347','#40E0D0','#EE82EE','#F5DEB3','#F5F5F5','#FFFF00','#9ACD32');
 
 Anitreemap.startColor=Math.floor(Math.random()*Anitreemap.colorset.length);
@@ -132,7 +131,7 @@ Anitreemap.prototype.serializeTreemap=function(treemapobj,parentsum,parentwidth,
 				var elewidth=obj.value/sum*width;
 				var eleheight=height;
 			}
-			var colorIndex=Anitreemap.currentColor+2;
+			var colorIndex=Anitreemap.currentColor+3;
 			if(colorIndex>Anitreemap.colorset.length){
 				colorIndex=colorIndex-Anitreemap.colorset.length;
 			}
@@ -145,8 +144,12 @@ Anitreemap.prototype.serializeTreemap=function(treemapobj,parentsum,parentwidth,
 			prevcolorIndex=colorIndex;
 			*/
 
-			html+='<div class="tagdiv" style="background-color:'+Anitreemap.colorset[colorIndex]+
-				'; line-height:'+eleheight+'px; width:'+elewidth+'px; height:'+eleheight+'px;">'+obj.name+'</div>';
+			html+='<div class="tagdiv" style="background-color:'+Anitreemap.colorset[colorIndex]+';';
+			if(colorIndex%2==1){
+				html+='color:white ;';
+			}
+			html+='line-height:'+eleheight+'px; width:'+elewidth+'px; height:'+eleheight+'px;">'+obj.name+'</div>';
+			
 		}
 		else{
 			html+=this.serializeTreemap(obj,sum,width,height,vertical);
